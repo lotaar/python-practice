a7e7f9cc0d20f6d6fe76eaf1161bfd1ba12ffec0
lotaar <remnantnogosu@gmail.com>
2024-11-14 22:58:22 +0300

    add random-number-generator solution

---
+++ random-number-generator/main.py
@@ -0,0 +1,6 @@
+import random
+
+min_num = int(input("Введите минимальное число: "))
+max_num = int(input("Введите максимальное число: "))
+
+print("Ваше случайное число: ", random.randint(min_num, max_num))
