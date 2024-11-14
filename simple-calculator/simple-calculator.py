17fbb41fef330d1c7d5486a975750c19a8fe8eb0
lotaar <remnantnogosu@gmail.com>
2024-11-14 22:14:30 +0300

    Added input hints

--- simple-calculator/simple-calculator.py
+++ simple-calculator/simple-calculator.py
@@ -1,5 +1,5 @@
-num1 = int(input())
-num2 = int(input())
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
 
 result = num1 + num2
 print("Результат сложения:", result)
