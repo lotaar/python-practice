f24572ab4673f9c2aed208b4b3dcaed9b4f8b7e0
lotaar <remnantnogosu@gmail.com>
2024-11-16 03:45:46 +0300

    add dog-age-converter solution

---
+++ dog-age-converter/main.py
@@ -0,0 +1,9 @@
+dog_years = int(input("Введите возраст собаки: "))
+FIRST_TWO_YEARS_RATE = 10.5
+OTHER_YEARS_RATE = 4
+
+if dog_years <= 2:
+    human_age = dog_years * FIRST_TWO_YEARS_RATE
+else:
+    human_age = (2 * FIRST_TWO_YEARS_RATE) + ((dog_years - 2) * OTHER_YEARS_RATE)
+print(f"Возраст собаки в человеческиx годах: {human_age}")
