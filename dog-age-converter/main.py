509d15e21b807574d0259e711df2a2c438814730
lotaar <remnantnogosu@gmail.com>
2024-11-16 05:01:13 +0300

    Correct grammar mistake

--- dog-age-converter/main.py
+++ dog-age-converter/main.py
@@ -6,4 +6,4 @@ if dog_years <= 2:
     human_age = dog_years * FIRST_TWO_YEARS_RATE
 else:
     human_age = (2 * FIRST_TWO_YEARS_RATE) + ((dog_years - 2) * OTHER_YEARS_RATE)
-print(f"Возраст собаки в человеческиx годах: {human_age}")
+print(f"Возраст собаки в человеческих годах: {human_age}")
